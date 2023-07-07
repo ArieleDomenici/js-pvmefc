17ab5479b7c89eb00365202ef946788a1af89f72
ArieleDomenici <30295861+ArieleDomenici@users.noreply.github.com>
2023-07-08 01:08:29 +0200

    esercizio1

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Test</h1>`;
+
+//esercizio provate a scrivere il codice per verificare titolo e autore di un certo volume contengono una certa sequenza di caratteri
+let titolo = 'RidsjTFwielfj';
+let autore = 'EoiwpoFDfewr';
+let stringToCheck = 'z';
+let contains = (titolo + autore).toLowerCase().includes(stringToCheck);
+console.log(contains);
