51746cea6acd429d4c9b808b273c23e42b208841
ArieleDomenici <30295861+ArieleDomenici@users.noreply.github.com>
2023-07-13 12:53:11 +0200

    1 file modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Test</h1>`;
+appDiv.innerHTML = `<h1> Test</h1>`;
 
 //esercizio provate a scrivere il codice per verificare titolo e autore di un certo volume contengono una certa sequenza di caratteri
 let titolo = 'RidsjTFwielfj';
